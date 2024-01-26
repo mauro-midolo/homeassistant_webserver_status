2619911dc9edacf442f4b526b5ca699f71f5e02a
Mauro Midolo <mmxx91@hotmail.it>
2024-01-26 20:24:34 +0000

    Fixed configuration

--- custom_components/webserver_status/__init__.py
+++ custom_components/webserver_status/__init__.py
@@ -1,8 +1,10 @@
 """The WebServer Status integration."""
 from .const import DOMAIN
 from homeassistant.config_entries import SOURCE_IMPORT
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
-async def async_setup(hass, config):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.async_create_task(
         hass.config_entries.flow.async_init(
             DOMAIN,
