0706c590718b5ddee9958498df208f62778e109b
Mauro Midolo <mmxx91@hotmail.it>
2024-01-26 21:30:46 +0000

    Fixed and test Web

--- custom_components/webserver_status/sensor.py
+++ custom_components/webserver_status/sensor.py
@@ -3,12 +3,19 @@ import logging
 import requests
 from datetime import timedelta
 from homeassistant.helpers.entity import Entity
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 SCAN_INTERVAL = timedelta(minutes=5)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    """Configura il componente all'interno di Home Assistant."""
+    _LOGGER.warning(entry)
+    async_add_entities([WebServerStatusSensor(entry)], True)
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the WebServer Status sensor."""
