4a75151440f2eb08019b37fb29735125d8f053bf
Mauro Midolo <mmxx91@hotmail.it>
2024-01-27 22:08:16 +0000

    Added CoordinatorEntity

--- custom_components/webserver_status/Ping.py
+++ custom_components/webserver_status/Ping.py
@@ -1,5 +1,4 @@
 class ConnectionStatus():
-    def __init__(self, hostname_alis, hostname, state, durantion):
-        self._hostname_alis = hostname_alis
+    def __init__(self, hostname, state, durantion):
         self._hostname = hostname
         self._data = {"state": state, "duration": durantion}
--- custom_components/webserver_status/sensor.py
+++ custom_components/webserver_status/sensor.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 from homeassistant.helpers.entity import Entity
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-
+from homeassistant.components.sensor import SensorEntity
 from .Ping import ConnectionStatus
 from .const import DOMAIN
 from homeassistant.helpers.entity import DeviceInfo
@@ -14,57 +14,54 @@ _LOGGER = logging.getLogger(__name__)
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 import time
 from .sensorlist import sensors_binary
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 class WebServerStatusDataCoordinator(DataUpdateCoordinator):
     """Class to manage fetching WebServer data."""
-    def __init__(self, hass, hostname_alis, hostname):
+    def __init__(self, hass, hostname):
         """Initialize the coordinator."""
-        super().__init__(hass, _LOGGER, name=hostname_alis, update_method=self._async_update_data, update_interval=30)
-        self.data : ConnectionStatus = ConnectionStatus(hostname_alis, hostname, None, None)
-
+        super().__init__(hass, _LOGGER, name=hostname, update_method=self._async_update_data, update_interval=30)
+        self._hostname = hostname
+        _LOGGER.warning("DATA!:" + self.data );
+ 
     async def _async_update_data(self):
         try:
             start_time = time.time()
-            response = response = await asyncio.to_thread(requests.get, self.data._hostname, timeout=5)
+            response = response = await asyncio.to_thread(requests.get, self._hostname, timeout=5)
             end_time = time.time()
-
+            state_result="offline"
             if response.status_code == 200:
-                self.data._data["state"] = "online"
-            else:
-                self.data._data["state"] = "offline"
-            self.data._data["response_time"] = end_time - start_time
+                state_result = "online"
+            duration_time = end_time - start_time
+            return ConnectionStatus(self._hostname, state_result, duration_time)
         except requests.RequestException as e:
-            _LOGGER.error(f"Error updating WebServer Status sensor: {e}")
-            self.data._data["state"] = "offline"
-            self.data._data["response_time"] = None
+            return ConnectionStatus(self._hostname, "offline", None)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
-    hostname_alis = entry.data.get('webserver_name', 'WebServer')
     webserver_url = entry.data.get('webserver_url', '')
-    coordinator = WebServerStatusDataCoordinator(hass, hostname_alis, webserver_url);
+    coordinator = WebServerStatusDataCoordinator(hass, webserver_url);
     await coordinator.async_config_entry_first_refresh();
     for sensor_name in sensors_binary:
-        async_add_entities([WebServerStatusSensor(sensor_name, coordinator)], True)
+        async_add_entities([WebServerStatusSensor(entry, sensor_name, coordinator)], True)
 
-class WebServerStatusSensor(Entity):
-    """Representation of a WebServer Status sensor."""
-
-    def __init__(self, sensor_name, coordinator: WebServerStatusDataCoordinator):
-        """Initialize the sensor."""
+class WebServerStatusEntity(CoordinatorEntity):
+    def __init__(self, entry: ConfigEntry, sensor_name, coordinator: WebServerStatusDataCoordinator):
+        super().__init__(coordinator)
+        self._entry = entry
         self._sensor_name = sensor_name
-        self._coordinator = coordinator
+
 
     @property
     def unique_id(self):
         """Return a unique ID to use for this entity."""
-        return f"{self._coordinator.data._hostname_alis}-{self._sensor_name}"
+        return f"{self.entry.data.get('webserver_name')}-{self._sensor_name}"
 
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return f"{self._coordinator.data._hostname_alis}-{self._sensor_name}"
+        return f"{self.entry.data.get('webserver_name')}-{self._sensor_name}"
 
     @property
     def state(self):
@@ -79,6 +76,10 @@ class WebServerStatusSensor(Entity):
     @property
     def device_info(self) -> DeviceInfo:
         return DeviceInfo(
-            name=self._coordinator.data._hostname_alis,
-            identifiers={(DOMAIN, self._coordinator.data._hostname)}
+            name=self.entry.data.get('webserver_name'),
+            identifiers={(DOMAIN, self._entry.data.get('webserver_url'))}
             )
+
+
+class WebServerStatusSensor(WebServerStatusEntity, SensorEntity):
+    """Representation of a WebServer Status sensor."""
