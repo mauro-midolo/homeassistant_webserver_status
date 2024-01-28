d5a9af90859f2f29e62fca67cf452bab0f71a768
Mauro Midolo <mmxx91@hotmail.it>
2024-01-28 14:01:21 +0000

    Changed variables

--- custom_components/webserver_status/http/httpclient.py
+++ custom_components/webserver_status/http/httpclient.py
@@ -10,12 +10,12 @@ class HttpClient():
     def get_request(self, url) -> ConnectionStatus:
         try:
             start_time = time.time()
-            response = response = requests.get( self._hostname, timeout=5)
+            response = response = requests.get( url, timeout=5)
             end_time = time.time()
             state_result="offline"
             if response.status_code == 200:
                 state_result = "online"
             duration_time = round(end_time - start_time, 2)
-            return ConnectionStatus(self._hostname, state_result, duration_time, response.status_code)
+            return ConnectionStatus(url, state_result, duration_time, response.status_code)
         except requests.RequestException:
-            return ConnectionStatus(self._hostname, "offline", None, None)
+            return ConnectionStatus(url, "offline", None, None)
