f87cbfb42a34bbc123463760a7a81ce75c87b980
Mauro Midolo <mmxx91@hotmail.it>
2024-01-28 14:13:31 +0000

    added first translation

--- custom_components/webserver_status/config_flow.py
+++ custom_components/webserver_status/config_flow.py
@@ -20,13 +20,6 @@ class WebServerStatusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     errors={"base": "invalid_url"},
                 )
 
-            # Check if there's already an entry with the same URL
-            #if self._async_current_entries():
-            #    return self.async_show_form(
-            #        step_id="user",
-            #        errors={"base": "url_already_exists"},
-            #    )
-
             # Configuration is valid, create an entry
             return self.async_create_entry(
                 title=user_input['webserver_name'],
