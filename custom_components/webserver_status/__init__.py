d6abcef2abc530ca4a2b5264ef1274896087b79f
Mauro Midolo <mauro.midolo@hotmail.com>
2025-12-11 11:49:01 +0100

    Implement options flow for web server status
    Added options flow handler for web server status.

--- custom_components/webserver_status/__init__.py
+++ custom_components/webserver_status/__init__.py
@@ -3,7 +3,14 @@ from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
 from .const import PLATFORMS
+from .config_flow import WebServerStatusOptionsFlowHandler
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+
+async def async_get_options_flow(config_entry: ConfigEntry):
+    """Return the options flow for this handler."""
+    return WebServerStatusOptionsFlowHandler(config_entry)
