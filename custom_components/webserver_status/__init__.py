0bfe62e9e006c52d00d14a756c0165494b771891
Mauro Midolo <mauro.midolo@hotmail.com>
2025-12-11 12:03:19 +0100

    Remove async_get_options_flow function
    Removed the async_get_options_flow function.

--- custom_components/webserver_status/__init__.py
+++ custom_components/webserver_status/__init__.py
@@ -9,8 +9,3 @@ from .config_flow import WebServerStatusOptionsFlowHandler
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
-
-
-async def async_get_options_flow(config_entry: ConfigEntry):
-    """Return the options flow for this handler."""
-    return WebServerStatusOptionsFlowHandler(config_entry)
