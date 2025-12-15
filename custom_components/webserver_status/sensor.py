0e71afd93a990c6d4919917c08090fe11873e668
Mauro Midolo <mauro.midolo@hotmail.com>
2025-12-15 09:55:21 +0100

    Fix get_request call to include hass parameter

--- custom_components/webserver_status/sensor.py
+++ custom_components/webserver_status/sensor.py
@@ -34,7 +34,7 @@ class WebServerStatusDataCoordinator(DataUpdateCoordinator):
 
     async def _async_update_data(self):
         http_client = HttpClient()
-        result = await http_client.get_request(self._hostname, self._ssl_checker)
+        result = await http_client.get_request(self.hass, self._hostname, self._ssl_checker)
     
         return {
             "state": result.state_result,
